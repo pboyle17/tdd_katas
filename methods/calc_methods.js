99712765a9e713c60e7510ff2951a56745dd0f4c
Paul Boyle <pboyle17@gmail.com>
2016-05-10 18:55:40 -0500

    finished first run through of the kata

--- methods/calc_methods.js
+++ methods/calc_methods.js
@@ -1,13 +1,16 @@
-function stringCalc (string){
-  var numberizedDigit = getNumberDigitOnesPlace(string);
+function stringCalc (arr){
+  var numbers = arr.map(function(number){
+    return convertMultipleDigitStringToNumber(number);
+  })
 
+  var answer = sum(numbers);
 
-  return numberizedDigit;
+  return answer;
 }
 
 function getNumberDigitTensPlace(string){
   var number;
-  if(string == ''){
+  if(string == '' || string == '0'){
     number = 0;
   }
   if(string == '1'){
@@ -41,7 +44,7 @@ function getNumberDigitTensPlace(string){
 
 function getNumberDigitOnesPlace (string){
   var number;
-  if(string == ''){
+  if(string == '' || string == '0'){
     number = 0;
   }
   if(string == '1'){
@@ -73,6 +76,40 @@ function getNumberDigitOnesPlace (string){
   return number;
 }
 
+function getNumberDigitHundredsPlace(string){
+  var number;
+  if(string == '' || string == '0'){
+    number = 0;
+  }
+  if(string == '1'){
+    number = 100;
+  }
+  if(string == '2'){
+    number = 200;
+  }
+  if(string == '3'){
+    number = 300;
+  }
+  if(string == '4'){
+    number = 400;
+  }
+  if(string == '5'){
+    number = 500;
+  }
+  if(string == '6'){
+    number = 600;
+  }
+  if(string == '7'){
+    number = 700;
+  }
+  if(string == '8'){
+    number = 800;
+  }if(string == '9'){
+    number = 900;
+  }
+  return number;
+}
+
 function sum(arr){
   var sum=0;
 
@@ -87,6 +124,10 @@ function convertMultipleDigitStringToNumber (string){
   var digits = string.split('');
   var count = digits.length;
   var number = digits.map(function(digit){
+    if(count == 3){
+      count--;
+      return getNumberDigitHundredsPlace(digit);
+    }
     if(count == 2){
       count--;
       return getNumberDigitTensPlace(digit);
@@ -103,6 +144,8 @@ function convertMultipleDigitStringToNumber (string){
   return converted;
 }
 
+
+module.exports.getNumberDigitHundredsPlace = getNumberDigitHundredsPlace;
 module.exports.sum = sum;
 module.exports.stringCalc = stringCalc;
 module.exports.getNumberDigitOnesPlace = getNumberDigitOnesPlace;
--- test/stringCalc_test.js
+++ test/stringCalc_test.js
@@ -1,27 +1,53 @@
 var expect = require('chai').expect;
 
-describe('stringCalc', function(){
+  describe('stringCalc', function(){
     var stringCalc = require('../methods/calc_methods').stringCalc;
 
   it('should return a number for an empty string',function(){
 
-    expect(stringCalc('')).to.be.a('number');
+    expect(stringCalc([''])).to.be.a('number');
   });
 
   it('should return zero for an empty string',function(){
 
-    expect(stringCalc('')).to.equal(0);
+    expect(stringCalc([''])).to.equal(0);
   });
 
   it('should return the number 1 for the string "1"',function(){
 
-    expect(stringCalc('1')).to.equal(1);
+    expect(stringCalc(['1'])).to.equal(1);
+  });
+
+  it('should return the number 0 for the string "0"',function(){
+
+    expect(stringCalc(['0'])).to.equal(0);
   });
 
   it('should return the number 9 for the string "9"',function(){
 
-    expect(stringCalc('9')).to.equal(9);
+    expect(stringCalc(['9'])).to.equal(9);
   });
+
+  it('should return number 54 for input string 54',function(){
+    expect(stringCalc(['54'])).to.equal(54);
+  });
+
+  it('should return number 00 for input string 00',function(){
+    expect(stringCalc(['00'])).to.equal(0);
+  });
+
+  it('should return 32 for the input strings 15 and 17',function(){
+    expect(stringCalc(['15','17'])).to.equal(32);
+  });
+
+  it('should return the number 132 for the input: ["132"]',function(){
+    expect(stringCalc(['132'])).to.equal(132);
+  });
+
+  it('should return the number 1459 for the input: ["500","659","300"]',function(){
+    expect(stringCalc(['500','659','300'])).to.equal(1459);
+  });
+
 });
 
 describe('getNumberDigitTensPlace',function(){
@@ -66,3 +92,15 @@ describe('convertMultipleDigitStringToNumber',function(){
     expect(convertMultipleDigitStringToNumber('12')).to.equal(12);
     });
   });
+
+describe('getNumberDigitHundredsPlace',function(){
+  var getNumberDigitHundredsPlace = require('../methods/calc_methods').getNumberDigitHundredsPlace;
+
+  it('should return 100 for the input 1',function(){
+    expect(getNumberDigitHundredsPlace('1')).to.equal(100);
+  });
+
+  it('should return 0 for the input 0',function(){
+    expect(getNumberDigitHundredsPlace('0')).to.equal(0);
+  });
+});
