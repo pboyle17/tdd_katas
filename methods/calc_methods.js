bd7b52c4dfe3326805e6573302dc7d75928949b5
Paul Boyle <pboyle17@gmail.com>
2016-05-10 16:46:05 -0500

    added some functions with unit testing included

--- methods/calc_methods.js
+++ methods/calc_methods.js
@@ -1,16 +1,103 @@
 function stringCalc (string){
-  var numberizedDigit = getNumberDigit(string);
+  var numberizedDigit = getNumberDigitOnesPlace(string);
+
 
   return numberizedDigit;
 }
 
-function getNumberDigit (string){
+function getNumberDigitTensPlace(string){
   var number;
   if(string == ''){
     number = 0;
   }
+  if(string == '1'){
+    number = 10;
+  }
+  if(string == '2'){
+    number = 20;
+  }
+  if(string == '3'){
+    number = 30;
+  }
+  if(string == '4'){
+    number = 40;
+  }
+  if(string == '5'){
+    number = 50;
+  }
+  if(string == '6'){
+    number = 60;
+  }
+  if(string == '7'){
+    number = 70;
+  }
+  if(string == '8'){
+    number = 80;
+  }if(string == '9'){
+    number = 90;
+  }
   return number;
 }
 
+function getNumberDigitOnesPlace (string){
+  var number;
+  if(string == ''){
+    number = 0;
+  }
+  if(string == '1'){
+    number = 1;
+  }
+  if(string == '2'){
+    number = 2;
+  }
+  if(string == '3'){
+    number = 3;
+  }
+  if(string == '4'){
+    number = 4;
+  }
+  if(string == '5'){
+    number = 5;
+  }
+  if(string == '6'){
+    number = 6;
+  }
+  if(string == '7'){
+    number = 7;
+  }
+  if(string == '8'){
+    number = 8;
+  }if(string == '9'){
+    number = 9;
+  }
+  return number;
+}
+
+function sum(arr){
+  return true;
+}
+
+function convertMultipleDigitStringToNumber (string){
+  var digits = string.split('');
+  var count = digits.length;
+  var number = digits.map(function(digit){
+    if(count == 2){
+      return getNumberDigitTensPlace(digit);
+    }
+    if(count ==1){
+      return getNumberDigitTensPlace(digit)
+    }
+
+    count--
+  });
+
+
+
+  return false;
+}
+
+module.exports.sum = sum;
 module.exports.stringCalc = stringCalc;
-module.exports.getNumberDigit = getNumberDigit;
+module.exports.getNumberDigitOnesPlace = getNumberDigitOnesPlace;
+module.exports.convertMultipleDigitStringToNumber = convertMultipleDigitStringToNumber;
+module.exports.getNumberDigitTensPlace = getNumberDigitTensPlace;
--- test/stringCalc_test.js
+++ test/stringCalc_test.js
@@ -12,4 +12,53 @@ describe('stringCalc', function(){
 
     expect(stringCalc('')).to.equal(0);
   });
+
+  it('should return the number 1 for the string "1"',function(){
+
+    expect(stringCalc('1')).to.equal(1);
+  });
+
+  it('should return the number 9 for the string "9"',function(){
+
+    expect(stringCalc('9')).to.equal(9);
+  });
 });
+
+describe('getNumberDigitTensPlace',function(){
+  var getNumberDigitTensPlace = require('../methods/calc_methods').getNumberDigitTensPlace;
+
+  it('should return the number 10 for string input 1',function(){
+    expect(getNumberDigitTensPlace(1)).to.equal(10);
+  });
+
+  it('should return the number 0 for the string input 0',function(){
+    expect(getNumberDigitTensPlace(0)).to.equal(0);
+  });
+});
+
+describe('getNumberDigitOnesPlace',function(){
+  var getNumberDigitOnesPlace = require('../methods/calc_methods').getNumberDigitOnesPlace;
+
+  it('should return the number 1 for input string 1',function(){
+    expect(getNumberDigitOnesPlace('1')).to.equal(1);
+  });
+  it('should return the number 0 for input empty string',function(){
+    expect(getNumberDigitOnesPlace('')).to.equal(0);
+  });
+});
+
+describe('sum',function(){
+  var sum = require('../methods/calc_methods').sum;
+
+  it('should return true',function(){
+    expect(sum([1,2])).to.equal(3);
+  });
+});
+
+describe('convertMultipleDigitStringToNumber',function(){
+  var convertMultipleDigitStringToNumber = require('../methods/calc_methods').convertMultipleDigitStringToNumber;
+
+  it('should convert the string 12 to the number 12',function(){
+    expect(convertMultipleDigitStringToNumber('12')).to.equal(12);
+    });
+  });
